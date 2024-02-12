ac49f266c5557ca0262712299b3a5b0cfc68585c
Samuel Essilfie <PsalmuelSelfie@gmail.com>
2024-02-12 03:28:33 +0000

    test: cors and middleware

--- src/app/FumScript.js
+++ src/app/FumScript.js
@@ -8,7 +8,7 @@ const FumsScript = () => {
 				src="http://cdn.scripture.api.bible/fums/fumsv2.min.js"
 				strategy="beforeInteractive"
 				integrity="1U6Q5HNn2sv4WQApQqkJo1rwImVwKJXPcFT5XYsQwgGA7+mQB18GzW0h0JEQnRSd%"
-				crossOrigin="anonymous"
+				// crossOrigin="anonymous"
 			/>
 		</>
 	);
